b32bc42a470fd5e27f2822e088d5fb448050d1e8
Scottiieee <splendidhoodies@gmail.com>
2025-07-25 22:36:18 +0800

    feat: use createBrowserRouter and setup routes

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,10 +1,56 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import App from "./App";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { MainLayout } from "./components/MainLayout";
+import { HomePage } from "./components/HomePage";
+import { StudentEntry } from "./components/StudentEntry";
+import { StudentData } from "./components/StudentData";
+import { StudentGradings } from "./components/StudentGradings";
+import { DataSummary } from "./components/DataSummary";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/main",
+    element: (
+      <ProtectedRoute>
+        <MainLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        index: true, // ✅ this means "/main" will render HomePage
+        element: <HomePage />,
+      },
+      {
+        path: "student-entry",
+        element: <StudentEntry />,
+      },
+      {
+        path: "student-data",
+        element: <StudentData />,
+      },
+      {
+        path: "student-gradings",
+        element: <StudentGradings />,
+      },
+      {
+        path: "data-summary",
+        element: <DataSummary />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
